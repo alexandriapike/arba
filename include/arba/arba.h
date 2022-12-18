4235409de6864fe410d3da283419dd4183ce9a38
alexandria <alexandria@alexandria-linux.org>
2022-12-17 17:13:00 -0800

    initial check in of the 'arba' library stub repository. This repository was automatically generated with the ought2make project by cloning it, running ./configure --libname=arba and then deleting the .git directory from ought2make and replacing it with a the project .git

---
+++ include/arba/arba.h
@@ -0,0 +1,18 @@
+#ifndef arba_H
+#define arba_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+/*	essential standard C library includes, data type declarations
+	and functon prototypes for an aought2make driven project go 
+	here i
+	replace arba_H with yourprojectname_H
+*/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
---
+++ tests/arba.c
@@ -0,0 +1,1 @@
+#include <arba/arba.h>
