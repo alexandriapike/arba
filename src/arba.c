09e0f580e61641d94d5103c7c3611d7774b6752c
alexandria <alexandria@alexandria-linux.org>
2022-12-20 10:56:29 -0800

    string to number works || printing number works || still has an unsafe conditional jump

--- src/arba.c
+++ src/arba.c
@@ -9,16 +9,14 @@ arba_fixed_point *add(arba_fixed_point *a, arba_fixed_point *b, arba_fixed_point
 arba_fixed_point *expand(arba_fixed_point *a, size_t length, size_t radix, int sign)
 	
 {
-	/* if a == NULL then this is new bignum */
+	/* if a == NULL then this is a new bignum */
 	/* so we may as well take the caller's size request */
 	/* and get out of here */
-
+	
 	if (a == NULL) {
 		a = malloc(sizeof(arba_fixed_point));
-		a->total_memory = length;
-		size_t *bb = a->datum;
-		bb = calloc(1, sizeof(size_t) * a->total_memory + 1);
-		a->datum = bb;
+		a->total_memory = length + 1;
+		a->datum = calloc(1, sizeof(size_t) * a->total_memory);
 		a->digits = a->total_memory;
 		return a;
 	} else {
@@ -33,16 +31,10 @@ arba_fixed_point *expand(arba_fixed_point *a, size_t length, size_t radix, int s
 	/* furthermore, to assume a memory request should be +1 is */
 	/* utter madness 999 * 999 = 998001 and transcendental parts are
 	 * determined by, and not to exceed the length of their inputs */
-		a->total_memory += length;
-		size_t *cc;
-		cc = a->datum;
-		cc = realloc(cc, sizeof(size_t) * a->total_memory);
-		a->datum =cc;
+		a->total_memory = length + 1;
+		a->datum = realloc(a->datum, sizeof(size_t) * a->total_memory);
 		a->digits = a->total_memory;
 	}
-	/* we assume the caller may be setting the radix or sign */
-	//a->radix = radix;
-	//a->sign = sign;
 	return a;
 	
 }
@@ -68,7 +60,7 @@ int arba_ascii_to_base(int letter)
 arba_fixed_point *arba_string_to_number(arba_fixed_point *f, char *s)
 {
 	size_t i = 0;
-	for (i = 0; s[i] != 0; ++i)
+	for (i =0 ; s[i] != 0; ++i)
 	{
 		if (s[i] == '.') {
 			f = expand(f, i, 0, 0);
@@ -82,9 +74,7 @@ arba_fixed_point *arba_string_to_number(arba_fixed_point *f, char *s)
 			f->sign = 1;
 		}
 		f = expand(f, i, 0, 0);
-
 		f->datum[i] = arba_ascii_to_base(s[i]);
-		//f->digits += 1;
 	}
 	return f;
 }
--- tests/arba.c
+++ tests/arba.c
@@ -1,13 +1,11 @@
 #include <arba/arba.h>
 #include <stdio.h>
+
 int main(void)
 {
-	FILE *fp = NULL;
 	arba_fixed_point *a = NULL;
 	a = arba_string_to_number(a, "123456");
-	
-	arba_print(fp, a);
+	arba_print(stdout, a);
 	return 0;
-
 }
 
